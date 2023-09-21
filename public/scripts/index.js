64e89aa1aa91565d4cbdf1510e17a42330909300
Rodrigo A Guspian <rodalejo8@gmail.com>
2023-09-20 22:11:19 -0500

    añadiendo validacioens al formulario

---
+++ public/scripts/index.js
@@ -0,0 +1,28 @@
+function validarFormulario() {
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    // Validación de nombre (no debe estar vacío)
+    if (nombre.trim() === "") {
+      alert("Por favor, ingresa tu nombre.");
+      return;
+    }
+
+    // Validación de correo electrónico (debe tener un formato válido)
+    const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!correoRegex.test(correo)) {
+      alert("Por favor, ingresa un correo electrónico válido.");
+      return;
+    }
+
+    // Validación de mensaje (no debe estar vacío)
+    if (mensaje.trim() === "") {
+      alert("Por favor, ingresa un mensaje.");
+      return;
+    }
+
+    // Si todas las validaciones pasan, puedes enviar el formulario aquí
+    alert("Formulario válido. Se enviará a continuación.");
+
+  }
